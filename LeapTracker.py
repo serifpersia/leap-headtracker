928b5d4c87b8ffb4082fa3528e62156ab37fa240
serifpersia <ramiserifpersia@gmail.com>
2024-05-28 15:11:31 +0200

    6 dof tracking

--- LeapTracker.py
+++ LeapTracker.py
@@ -2,8 +2,6 @@ import cv2
 import numpy as np
 import leapuvc
 import mediapipe as mp
-import math
-import time
 import struct
 import socket
 
@@ -19,21 +17,78 @@ def apply_zoom(image, zoom_factor):
     resized_image = cv2.resize(cropped_image, (width, height), interpolation=cv2.INTER_LINEAR)
     return resized_image
 
-def add_pose_info_text(image, x, y, z):
-    text_color = (255, 255, 255)  # White color
+# Function to handle mouse click events
+def on_click(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN and 100 <= x <= 200 and 350 <= y <= 400:
+        if not is_settings_window_open():
+            # Define Various Camera Control Settings
+            cv2.namedWindow('Settings')
+            cv2.resizeWindow('Settings', 410, 310)
+            cv2.createTrackbar('Exposure', 'Settings', exposure_level, 32222, leap.setExposure)  # Sets the exposure time in microseconds
+            cv2.createTrackbar('LEDs', 'Settings', leds_on, 1, lambda a: (leap.setLeftLED(a), leap.setCenterLED(a), leap.setRightLED(a)))  # Turns on the IR LEDs
+            cv2.createTrackbar('Gamma', 'Settings', gamma_on, 1, leap.setGammaEnabled)  # Applies a sqrt(x) contrast-reducing curve in 10-bit space
+            cv2.createTrackbar('Anlg Gain', 'Settings', analog_gain_level, 63, leap.setGain)  # Amplifies the signal in analog space, 16-63
+            cv2.createTrackbar('Dgtl Gain', 'Settings', digital_gain_level, 16, leap.setDigitalGain)  # Digitally amplifies the signal in 10-bit space
+            cv2.createTrackbar('HDR', 'Settings', hdr_on, 1, leap.setHDR)  # Selectively reduces the exposure of bright areas at the cost of fixed-pattern noise
+            cv2.createTrackbar('Rotate', 'Settings', rotate_on, 1, leap.set180Rotation)  # Rotates each camera image in-place 180 degrees (need to unflip when using calibrations!)
+            cv2.createTrackbar('Zoom', 'Settings', int(zoom_level - 1), 10, lambda x: None)  # Zoom level
+
+  
+# Function to add pose information text to the image
+def add_pose_info_text(image, x, y, z, pitch, yaw, roll):
+    text_color = (0, 255, 0)  # White color
     font = cv2.FONT_HERSHEY_SIMPLEX
     font_scale = 1
     thickness = 4
-    
-    cv2.putText(image, f"pitch: {np.round(x, 2)}", (15, 50), font, font_scale, text_color, thickness)
-    cv2.putText(image, f"yaw: {np.round(y, 2)}", (15, 100), font, font_scale, text_color, thickness)
-    cv2.putText(image, f"roll: {np.round(z, 2)}", (15, 150), font, font_scale, text_color, thickness)
 
-# Initialize Mediapipe face mesh
-mp_face_mesh = mp.solutions.face_mesh
-face_mesh = mp_face_mesh.FaceMesh(min_detection_confidence=0.7, min_tracking_confidence=0.7)
-mp_drawing = mp.solutions.drawing_utils
-drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+    cv2.putText(image, f"X: {np.round(x, 2)}", (15, 50), font, font_scale, text_color, thickness)
+    cv2.putText(image, f"Y: {np.round(y, 2)}", (15, 100), font, font_scale, text_color, thickness)
+    cv2.putText(image, f"Z: {np.round(z, 2)}", (15, 150), font, font_scale, text_color, thickness)
+    cv2.putText(image, f"Pitch: {np.round(pitch, 2)}", (15, 200), font, font_scale, text_color, thickness)
+    cv2.putText(image, f"Yaw: {np.round(yaw, 2)}", (15, 250), font, font_scale, text_color, thickness)
+    cv2.putText(image, f"Roll: {np.round(roll, 2)}", (15, 300), font, font_scale, text_color, thickness)
+
+# Function to calculate pitch, yaw, and roll angles
+def calculate_pose(landmarks):
+    # Define the reference points for the face
+    nose_tip = landmarks[2]
+    left_eye = landmarks[33]
+    right_eye = landmarks[263]
+
+    # Calculate the vectors representing the orientation of the face
+    v1 = np.array([left_eye[0] - right_eye[0], left_eye[1] - right_eye[1], left_eye[2] - right_eye[2]])
+    v2 = np.array([nose_tip[0] - (left_eye[0] + right_eye[0]) / 2, 
+                   nose_tip[1] - (left_eye[1] + right_eye[1]) / 2, 
+                   nose_tip[2] - (left_eye[2] + right_eye[2]) / 2])
+
+    # Calculate yaw, pitch, and roll angles
+    yaw = -np.arctan2(v1[2], v1[0]) * 180 / np.pi
+    pitch = np.arctan2(v2[1], v2[2]) * 180 / np.pi
+    roll = np.arctan2(v2[0], v2[2]) * 180 / np.pi
+
+    return pitch, yaw, roll
+
+
+# Function to extract position (x, y, z) of the face
+def extract_position(landmarks):
+    # Extract the coordinates of the nose tip
+    nose_tip = landmarks[2]
+    left_eye = landmarks[33]
+    right_eye = landmarks[263]
+    
+    # Calculate the center point between the eyes (x, y)
+    center_x = (left_eye[0] + right_eye[0]) / 2
+    center_y = (left_eye[1] + right_eye[1]) / 2
+    
+    # Adjust x and y based on the center point between the eyes
+    x = nose_tip[0] - center_x * 100
+    y = nose_tip[1] - center_y * 100
+    
+    # Z coordinate remains the same
+    z = nose_tip[2] * 1000
+
+    return x, y, z
+
 
 # Start the Leap Capture Thread
 leap = leapuvc.leapImageThread()
@@ -53,23 +108,6 @@ hdr_on = 0
 rotate_on = 1
 zoom_level = 3
 
-
-# Function to handle mouse click events
-def on_click(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONDOWN and 100 <= x <= 200 and 350 <= y <= 400:
-        if not is_settings_window_open():
-            # Define Various Camera Control Settings
-            cv2.namedWindow('Settings')
-            cv2.resizeWindow('Settings', 410, 310)
-            cv2.createTrackbar('Exposure', 'Settings', exposure_level, 32222, leap.setExposure)  # Sets the exposure time in microseconds
-            cv2.createTrackbar('LEDs', 'Settings', leds_on, 1, lambda a: (leap.setLeftLED(a), leap.setCenterLED(a), leap.setRightLED(a)))  # Turns on the IR LEDs
-            cv2.createTrackbar('Gamma', 'Settings', gamma_on, 1, leap.setGammaEnabled)  # Applies a sqrt(x) contrast-reducing curve in 10-bit space
-            cv2.createTrackbar('Anlg Gain', 'Settings', analog_gain_level, 63, leap.setGain)  # Amplifies the signal in analog space, 16-63
-            cv2.createTrackbar('Dgtl Gain', 'Settings', digital_gain_level, 16, leap.setDigitalGain)  # Digitally amplifies the signal in 10-bit space
-            cv2.createTrackbar('HDR', 'Settings', hdr_on, 1, leap.setHDR)  # Selectively reduces the exposure of bright areas at the cost of fixed-pattern noise
-            cv2.createTrackbar('Rotate', 'Settings', rotate_on, 1, leap.set180Rotation)  # Rotates each camera image in-place 180 degrees (need to unflip when using calibrations!)
-            cv2.createTrackbar('Zoom', 'Settings', int(zoom_level - 1), 10, lambda x: None)  # Zoom level
-          
 # Create main window with dark gray background
 window = np.full((450, 300, 3), 255, dtype=np.uint8)
 
@@ -84,128 +122,82 @@ text_x = int((200 + 100 - text_size[0]) / 2)
 text_y = int((400 + 350 + text_size[1]) / 2)
 cv2.putText(window, text, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, text_color, 1, cv2.LINE_AA)
 
-# Initialize calibration variables
-calibrated = False
-calibration_offset = np.zeros(3)
-
 # UDP settings
 udp_address = ("127.0.0.1", 4242)
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Capture images until 'q' is pressed
-while (not (cv2.waitKey(1) & 0xFF == ord('q'))) and leap.running:
-    newFrame, leftRightImage = leap.read()
-    if newFrame:
-        if is_settings_window_open():
-            # Update settings values
-            exposure_level = cv2.getTrackbarPos('Exposure', 'Settings')
-            leds_on = cv2.getTrackbarPos('LEDs', 'Settings')
-            gamma_on = cv2.getTrackbarPos('Gamma', 'Settings')
-            analog_gain_level = cv2.getTrackbarPos('Anlg Gain', 'Settings')
-            digital_gain_level = cv2.getTrackbarPos('Dgtl Gain', 'Settings')
-            hdr_on = cv2.getTrackbarPos('HDR', 'Settings')
-            rotate_on = cv2.getTrackbarPos('Rotate', 'Settings')
-            zoom_level = cv2.getTrackbarPos('Zoom', 'Settings') + 1.0
-            
-        # Apply zoom to the left camera image
-        zoomed_image = apply_zoom(leftRightImage[0], zoom_level)
-        # Resize the Leap image to match the area in the window
-        resized_image = cv2.resize(zoomed_image, (200, 200))
-        # Convert the resized Leap image to 3-channel
-        resized_image_3ch = cv2.cvtColor(resized_image, cv2.COLOR_GRAY2BGR)
-        # Display the resized frame in the window
-        # Perform face detection
+# Initialize Mediapipe face mesh
+mp_face_mesh = mp.solutions.face_mesh
+face_mesh = mp.solutions.face_mesh
+mp_drawing = mp.solutions.drawing_utils
+drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 
-        zoomed_image_bgr = cv2.cvtColor(zoomed_image, cv2.COLOR_GRAY2BGR)
-        
-        results = face_mesh.process(zoomed_image_bgr)
-        
-        img_h, img_w, _ = zoomed_image_bgr.shape
-        face_3d = []
-        face_2d = []
+with mp_face_mesh.FaceMesh(
+    max_num_faces=1,
+    refine_landmarks=True,
+    min_detection_confidence = 0.5,
+    min_tracking_confidence = 0.5) as face_mesh:
+    
+    # Capture images until 'q' is pressed
+    while (cv2.waitKey(1) & 0xFF != ord('q')) and leap.running:
+        new_frame, left_right_image = leap.read()
+        if new_frame:
+            if is_settings_window_open():
+                # Update settings values
+                exposure_level = cv2.getTrackbarPos('Exposure', 'Settings')
+                leds_on = cv2.getTrackbarPos('LEDs', 'Settings')
+                gamma_on = cv2.getTrackbarPos('Gamma', 'Settings')
+                analog_gain_level = cv2.getTrackbarPos('Anlg Gain', 'Settings')
+                digital_gain_level = cv2.getTrackbarPos('Dgtl Gain', 'Settings')
+                hdr_on = cv2.getTrackbarPos('HDR', 'Settings')
+                rotate_on = cv2.getTrackbarPos('Rotate', 'Settings')
+                zoom_level = cv2.getTrackbarPos('Zoom', 'Settings') + 1.0
 
-        if results.multi_face_landmarks:
-            for face_landmarks in results.multi_face_landmarks:
-                for idx, lm in enumerate(face_landmarks.landmark):
-                    if idx in [33, 263, 1, 61, 291, 199]:  # Specific landmarks for pose estimation
-                        if idx == 1:
-                            nose_2d = (lm.x * img_w, lm.y * img_h)
-                            nose_3d = (lm.x * img_w, lm.y * img_h, lm.z * 3000)
+            # Apply zoom to the left camera image
+            zoomed_image = apply_zoom(left_right_image[0], zoom_level)
+            # Resize the Leap image to match the area in the window
+            resized_image = cv2.resize(zoomed_image, (200, 200))
 
-                        x, y = int(lm.x * img_w), int(lm.y * img_h)
+            # Perform face detection
+            image = cv2.cvtColor(zoomed_image, cv2.COLOR_GRAY2RGB)
 
-                        # Get the 2D Coordinates
-                        face_2d.append([x, y])
+            results = face_mesh.process(image)
 
-                        # Get the 3D Coordinates
-                        face_3d.append([x, y, lm.z])
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-                # Convert it to the NumPy array
-                face_2d = np.array(face_2d, dtype=np.float64)
-                face_3d = np.array(face_3d, dtype=np.float64)
+            if results.multi_face_landmarks:
+                for face_landmarks in results.multi_face_landmarks:
+                    # Convert landmarks into list of tuples
+                    landmark_coords = [(landmark.x, landmark.y, landmark.z) for landmark in face_landmarks.landmark]
 
-                # The camera matrix
-                focal_length = 1 * img_w
+                    # Calculate pose angles
+                    pitch, yaw, roll = calculate_pose(landmark_coords)
 
-                cam_matrix = np.array([[focal_length, 0, img_h / 2],
-                                       [0, focal_length, img_w / 2],
-                                       [0, 0, 1]])
+                    # Calculate position coordinates
+                    x, y, z = extract_position(landmark_coords)
 
-                # The distortion parameters
-                dist_matrix = np.zeros((4, 1), dtype=np.float64)
+                    data = [x, y, z, yaw, pitch, roll]
+                    buf = struct.pack('dddddd', *data)
+                    udp_socket.sendto(buf, udp_address)
 
-                # Solve PnP
-                success, rot_vec, trans_vec = cv2.solvePnP(face_3d, face_2d, cam_matrix, dist_matrix)
+                    # Draw landmarks on the face
+                    mp_drawing.draw_landmarks(
+                        image = image,
+                        landmark_list = face_landmarks,
+                        connections=mp_face_mesh.FACEMESH_TESSELATION,
+                        landmark_drawing_spec=drawing_spec,
+                        connection_drawing_spec=drawing_spec
+                    )
+                    
+                    # Add pose info text to the image
+                    add_pose_info_text(image,x, y, z, pitch, yaw, roll)
 
-                # Check for calibration reset key
-                if cv2.waitKey(1) & 0xFF == ord('r'):
-                    calibrated = True
-                    calibration_offset = rot_vec.flatten()
-                    print("Calibration reset")
+            # Resize the zoomed image with Mediapipe overlay
+            resized_image_with_overlay = cv2.resize(image, (200, 200))
 
-                # Apply calibration offset if calibrated
-                if calibrated:
-                    rot_vec = rot_vec.flatten() - calibration_offset
-                    rot_vec = rot_vec.reshape((3, 1))
+            window[100:300, 50:250] = resized_image_with_overlay
+            cv2.imshow('LeapTracker', window)
+            cv2.setMouseCallback('LeapTracker', on_click)
 
-                # Get rotational matrix
-                rmat, jac = cv2.Rodrigues(rot_vec)
-
-                # Get angles
-                angles, mtxR, mtxQ, Qx, Qy, Qz = cv2.RQDecomp3x3(rmat)
-
-                # Send orientation and positional data over UDP
-                x, y, z = angles[0] * 360, angles[1] * 360, angles[2] * 360  # Extracted angles
-                data = [0, 0, 0, y, x, z]  # Zeros for X, Y, Z positional data
-                buf = struct.pack('dddddd', *data)
-                udp_socket.sendto(buf, udp_address)
-
-
-                # Display the nose direction
-                nose_3d_projection, jacobian = cv2.projectPoints(nose_3d, rot_vec, trans_vec, cam_matrix, dist_matrix)
-
-                p1 = (int(nose_2d[0]), int(nose_2d[1]))
-                p2 = (int(nose_2d[0] + y * 10), int(nose_2d[1] - x * 10))
-
-                cv2.line(zoomed_image_bgr, p1, p2, (255, 0, 0), 3)
-
-                # Add the text on the image
-                add_pose_info_text(zoomed_image_bgr, x, y, z)
-                
-                # Draw landmarks on the face
-                mp_drawing.draw_landmarks(
-                    image=zoomed_image_bgr,
-                    landmark_list=face_landmarks,
-                    connections=mp_face_mesh.FACEMESH_TESSELATION,
-                    landmark_drawing_spec=drawing_spec,
-                    connection_drawing_spec=drawing_spec)
-                
-        # Resize the zoomed image with Mediapipe overlay
-        resized_image_with_overlay = cv2.resize(zoomed_image_bgr, (200, 200))
-        
-        window[100:300, 50:250] = resized_image_with_overlay
-        cv2.imshow('LeapTracker', window)
-        cv2.setMouseCallback('LeapTracker', on_click)
-        
-# Close all windows
-cv2.destroyAllWindows()
+    # Close all windows
+    cv2.destroyAllWindows()
